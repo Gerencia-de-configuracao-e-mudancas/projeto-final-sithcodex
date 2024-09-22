b8360502e427045b4e1a3db49829438d61e23d9b
Lucas Paulo <lp152659@gmail.com>
2024-09-22 19:12:23 -0300

    Reorganização de pastas e fixs de pequenos erros

--- js/pag_animais.js
+++ js/pag_animais.js
@@ -3,7 +3,7 @@ const buttoms = document.querySelectorAll('.btn');
 buttoms.forEach(function(buttom){
     buttom.addEventListener('click', () => {
         const som = buttom.getAttribute('data-som');
-
+        
         const audio = new Audio(som);
         audio.play();
     })
